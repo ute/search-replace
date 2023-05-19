f264cb960b73ebd04d35d8a299968a9ecf24a043
Ute Hahn <utelity@gmail.com>
2023-05-19 19:51:01 +0200

    Initial commit (to gh)
    new version of "simple-abbreviations" example. Decided not to allow top level search-replace pairs

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# search-replace
-quarto filter extension for simple search-replace macros
+# Search-replace Extension for Quarto
+
+Quarto filter extension that lets you automatically search and replace strings in quarto document when rendering, in a similar way as simple LaTeX macros.
+
+Define pairs of search string and replacement in the document yaml, and let quarto do the work. The filter searches document text, including link targets. Replacements may include markdown text modifiers (emphasis) and math inline formulae.
+
+
+## Installing
+
+```bash
+quarto add ute/search-replace
+```
+
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+## Using
+
+Include the filter in the document's yaml, and add a key `search-replace`. Under this key, define your search-replace pairs as subkeys in the form `search : replace`. It is a good idea to start the search key with a special character, such as "+" or ".", although it is not required.
+
+Upon rendering, every string that is a search key will be replaced. Therefore, keys should not be contained in each other - this would give ambiguous results.
+
+### Example:
+With the yaml
+```yaml
+---
+filters:
+  - search-replace
+search-replace:
+  +quarto: "[Quarto](https://quarto.org)"
+  +qurl  : https://quarto.org/docs
+  +forml : $\alpha * \beta = \gamma$
+  +pyth  : "Pythagoras' theorem: $a^2 + b^2 = \\dots$"
+  .doo   : "- doodledoo - "
+  +dab   : "**dab**"
+  "!doa" : "`duaaah`"
+  +br    : <br>\newline
+---  
+```
+document text
+```md
++quarto allows us to write beautiful texts 
+about +pyth or similar complicated formulas (e.g. +forml), 
+and to [create our *own* filters](+qurl/extensions/filters.html). +br
+Even extensions that replace text:+br
+.doo+dab+dab!doa, +dab!doa!
+```
+gets rendered as
+
+> [Quarto](https://quarto.org) allows us to write beautiful texts about Pythagoras' theorem: $a^2 + b^2 = \dots$ or similar complicated formulas (e.g. $\alpha *\beta=\gamma$),  
+and to [create our *own* filters](https://quarto.org/docs/extensions/filters.html). <br>\newline 
+Even extensions that replace text:<br>\newline
+- doodledoo -**dab****dab**`duaaah`, **dab**`duaaah`!
+
+## Known quirks
+
+- search keys and replacement strings that can be interpreted as yaml because of special characters such as `:`,  or leading  `-`,`` ` ``,`[`, `!`, have to be enquoted. To be on the safe side, you can just enquote all replacement strings.
+- backslashes in LaTeX formulae have to be escaped with a second backslash if and only if the containing string is enquoted.
+
+## Acknowledgement
+
+ [Scott Koga-Browes](https://github.com/scokobro)' lovely python-based pandoc filter [pandoc-abbreviations](https://github.com/scokobro/pandoc-abbreviations) that replaces text strings (not links) inspired me to write this quarto extension. He requires the search keys to start with a "+" sign which gives a nice and clear appearance - they are easy to spot in the source text.
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](example.qmd).
+
---
+++ _extensions/search-replace/search-replace.lua
@@ -0,0 +1,150 @@
+
+-- filter for abbreviations, also replaces in links
+-- not really elegantly programmed, but seems to do its job :-)
+--[[
+MIT License
+
+Copyright (c) 2023 Ute Hahn
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]--
+
+-- local pout=quarto.log.output
+-- local str= pandoc.utils.stringify
+-- local ptyp = pandoc.utils.type
+
+local abbreviations = {}
+local abbr ={}
+
+
+-- helper function, for readability
+local function append(lst1, lst2)
+  for _, v in ipairs(lst2) do
+    table.insert(lst1, v)
+  end 
+  return(lst1)
+end  
+
+local function replacevalues(lst1, lst2)
+  for i, v in ipairs(lst2) do
+    lst1[i]= v
+  end 
+  return(lst1)
+end  
+
+
+-- function that takes a string and a list of patterns
+--- returns: first occurrence of any pattern, or nil
+---  start, end, pattern
+local findfirst = function(strg, patts)
+  if strg == nil or patts == nil then return nil end
+  if #strg == 0 or #patts == 0 then return nil end
+  local a, b, n = 0, 0, #strg + 1
+  local A = n
+  local Patt = ""
+  for i, patt in pairs(patts) do
+     a, b = string.find(strg, patt)
+     if a then
+       if a < A then
+         A = a
+         B = b
+         Patt = patt
+       end   
+     end   
+  end
+  if A < n then return A, B, Patt
+    else return nil 
+  end    
+end
+
+
+-- Todo-if-bored: rewrite, just replacing from back to front
+local  replace_abbr_in_str = function(original)
+  local result = pandoc.Inlines("")    
+  local istart, iend, patt = findfirst(original, abbr)
+  local found = false
+  if istart then 
+    found = true
+    while found do 
+      if istart > 1 then -- preserve start
+        local startstr = string.sub(original, 1, istart-1)
+        table.insert(result,  pandoc.Str(startstr))
+      end
+      append(result, abbreviations[patt])
+      original = string.sub(original, iend+1, #original)
+      istart, iend, patt = findfirst(original, abbr)
+      found = istart ~= nil
+    end
+    if #original > 0 then table.insert(result, pandoc.Str(original)) end
+    return(result)
+  else
+    return(pandoc.Inlines(original))
+  end     
+end   
+
+replace_abbr = {
+  Inlines=function(ilist)
+    local newtable =  pandoc.Inlines("") 
+    local zzz = pandoc.Inlines("") 
+    for i, v in ipairs(ilist) do
+       if v.t == "Str" then
+          zzz = replace_abbr_in_str(v.text)
+       elseif v.t == "Link" then
+          v.target = pandoc.utils.stringify(replace_abbr_in_str(pandoc.utils.stringify(v.target)))
+          zzz = pandoc.Inlines(v)  
+       else  zzz = pandoc.Inlines(v)  
+       end
+      newtable = append(newtable, zzz)
+    end
+   return(newtable)
+  end
+}
+
+
+return{
+{
+ Meta = function(m)
+  local abbrdefs = m["search-replace"]
+   i = 1
+   for k, v in pairs(m) do
+     if string.sub(k, 1, 1) == "+" then 
+        abbreviations[k] = v
+        abbr[i] = k
+        i = i+1
+      end
+   end
+  if abbrdefs then 
+    for k, v in pairs(abbrdefs) do
+         abbreviations[k] = v
+         abbr[i] = k
+         i = i+1
+    end
+  end
+  return(m)
+  end
+} 
+
+, {
+Pandoc = function(doc)
+  return(pandoc.Pandoc(doc.blocks:walk(replace_abbr), doc.meta))
+end  
+}
+}
+
+
